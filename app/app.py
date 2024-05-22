33ee68a1c43910a455ccb9e018622da6ac0b3a24
Hans Missenheim <hansmissenheim@gmail.com>
2024-05-22 13:07:53 -0400

    Use loaded model to predict play from form values

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from model import load_model
 
 app = Flask(__name__)
 
@@ -10,5 +11,20 @@ def index():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    model_input = request.form
-    return "Pass"
+    data = [
+        int(request.form.get("drive", 1)),
+        int(request.form.get("qtr", 1)),
+        int(request.form.get("quarter_seconds_remaining", 900)),
+        int(request.form.get("down", 1)),
+        int(request.form.get("ydstogo", 10)),
+        int(request.form.get("yardline_100", 75)),
+        int(request.form.get("shotgun", 0)),
+        int(request.form.get("no_huddle", 0)),
+        int(request.form.get("score_differential", 0)),
+        int(request.form.get("spread_line", 0)),
+        int(request.form.get("season", 2023)),
+    ]
+    xgb = load_model()
+    play_type = xgb.predict([data])
+
+    return "Pass" if play_type[0] == 0 else "Run"
