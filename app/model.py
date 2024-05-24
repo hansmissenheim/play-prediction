3f62d33ebf72ad1ed64d92dee4f8e5dfff254099
Hans Missenheim <hansmissenheim@gmail.com>
2024-05-24 17:00:26 -0400

    Add load_model method to load on instantiation

--- app/model.py
+++ app/model.py
@@ -14,11 +14,11 @@ class PlayPredictionModel:
     def __new__(cls):
         if not cls._instance:
             cls._instance = super().__new__(cls)
+            cls._instance.model = cls.load_model()
         return cls._instance
 
-
-def load_model():
-    model = XGBClassifier()
-    model.load_model(MODEL_PATH)
-
-    return model
+    @classmethod
+    def load_model(cls):
+        model = XGBClassifier()
+        model.load_model(MODEL_PATH)
+        return model
