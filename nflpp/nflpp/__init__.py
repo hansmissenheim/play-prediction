a4ce9e548bc5e1c6a0ccb3677859db72ca8d6f48
Hans Missenheim <hansmissenheim@gmail.com>
2024-06-09 22:44:44 -0400

    Add function for plotting confusion matrix

--- nflpp/nflpp/__init__.py
+++ nflpp/nflpp/__init__.py
@@ -1,3 +1,4 @@
 from .dataprep import load_pbp_data
+from .plot import plot_confusion_matrix
 
-__all__ = ["load_pbp_data"]
+__all__ = ["load_pbp_data", "plot_confusion_matrix"]
---
+++ nflpp/nflpp/plot.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
+
+AXIS_LABELS = ["Pass", "Run"]
+
+Array = pd.Series | np.ndarray
+
+
+def plot_confusion_matrix(targets: Array, predictions: Array):
+    cm = confusion_matrix(targets, predictions)
+    plt.imshow(cm, interpolation="nearest")
+
+    plt.title("Confusion Matrix")
+    plt.xlabel("Predicted Play Call")
+    plt.ylabel("True Play Call")
+    plt.xticks([0, 1], AXIS_LABELS)
+    plt.yticks([0, 1], AXIS_LABELS)
+
+    for i in range(2):
+        for j in range(2):
+            plt.text(j, i, cm[i, j], horizontalalignment="center")
+
+    plt.show()
