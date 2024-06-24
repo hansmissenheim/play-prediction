092a254e0d7d1afbf0c5882ae616bf79ea687018
Hans Missenheim <hansmissenheim@gmail.com>
2024-06-24 16:20:19 -0400

    Add betting odds calculation function

--- app/utils.py
+++ app/utils.py
@@ -7,3 +7,10 @@ encodings = None
 if (DATA_DIR / "encodings.json").exists():
     with open(DATA_DIR / "encodings.json", "r") as f:
         encodings = json.load(f)
+
+
+def betting_odds(probability: float) -> int:
+    if probability > 0.5:
+        return round(-100 * (probability / (1 - probability)))
+    else:
+        return round(100 * ((1 - probability) / probability))
